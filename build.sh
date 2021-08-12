baad7eda98c2f2fb67bfb870cb87a9379c399569
jichan <joseph@jc-lab.net>
2021-08-12 13:31:25 +0900

    auto detect image tag

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-linuxkit pkg build --hash-path . -network -org jclab pkg/linuxkit-bitnami-openldap
+linuxkit pkg build -network -org jclab pkg/linuxkit-bitnami-openldap
+sed -e "s|<OPENLDAP_IMAGE>|$(linuxkit pkg show-tag -org jclab pkg/linuxkit-bitnami-openldap)|g" openldap.yml.in > openldap.yml
 securekit-build.sh openldap
